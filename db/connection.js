00a9625e3ed482a39e02958459fd872463be850b
Vaibhav Choudhary <79092914+choudhary-vaibhav@users.noreply.github.com>
2022-09-01 13:52:31 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb+srv://vaibhav:cooL123@cluster0.xtdfkmp.mongodb.net/pizzadb?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://username:password@cluster0.xtdfkmp.mongodb.net/pizzadb?retryWrites=true&w=majority';
 
 mongoose.connect(URL, {maxPoolSize:5}, (err) => {
     if(err){
